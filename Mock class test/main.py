87e78e5e08492c0a4849eec9ccb8ced18c8631ed
deeptivij1 <v2957d@stge.org.uk>
2024-10-03 12:53:32 +0000

    finished part c

--- Mock class test/main.py
+++ Mock class test/main.py
@@ -1,20 +1,24 @@
 import csv 
 
 class Order():
-    def __init__(self, name, product, price):
+    def __init__(self, id, name, product, amountSpent, category):
+        self.id = id 
         self.name = name
         self.product = product
-        self.price = price
+        self.amountSpent = amountSpent
+        self.category = category
+        
+
 
 
 def readOrdersFromCSV():
 
-    with open('Mock class test/orders.csv', 'r') as f:
+    with open('Mock class test/ordersExtended.csv', 'r') as f:
         orders = [] # array of records
         reader = csv.reader(f) # premade reader using f to refer to file
         next(reader)
         for row in reader:
-            order_row = Order(row[0], row[1], float(row[2]))
+            order_row = Order(row[0], row[1], (row[2]), float(row[3]), row[4])
             orders.append(order_row)
     
     return orders
@@ -24,10 +28,25 @@ def readOrdersFromCSV():
 def findMaxOrderWithTv(orders):
     maxOrder = orders[0] # setting highest order to whole of record 1
     for order in orders:
-        if "TV" in order.product and order.price > maxOrder.price: #if current order is a TV and it's price > maxOrder price
+        if "TV" in order.product and order.amountSpent > maxOrder.amountSpent: #if current order is a TV and it's price > maxOrder price
             maxOrder = order # change the max record to the current record
 
-    print(maxOrder.price) # just print price of max record
+    print(maxOrder.amountSpent) # just print price of max record
 
-orders = readOrdersFromCSV()
-findMaxOrderWithTv(orders)
+def findDiscounts(orders):
+    with open('Mock class test/discounts.txt', 'w') as file:
+        for order in orders:
+            if int(order.id) % 5 == 0:
+                file.write(order.id + "-" + order.product[:3] + "-DISCOUNT CODE ASSIGNED" + "\n")
+            else:
+                file.write(order.id + "-" + order.product[:3] + "-NO DISCOUNT" + "\n")
+
+
+
+def main():
+    orders = readOrdersFromCSV()
+    findMaxOrderWithTv(orders)
+    findDiscounts(orders)
+
+
+main()
