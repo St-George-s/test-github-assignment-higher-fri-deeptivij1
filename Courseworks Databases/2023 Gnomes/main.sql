8e0b96c4a938da7d027227fcf36bee685e2b3c22
deeptivij1 <v2957d@stge.org.uk>
2024-11-21 14:19:09 +0000

    2a

--- Courseworks Databases/2023 Gnomes/main.sql
+++ Courseworks Databases/2023 Gnomes/main.sql
@@ -1,1 +1,13 @@
-DESCRIBE ALL TABLES;
+-- -- 2b: Lists of gnomeNames and quantities with solar in description
+-- SELECT gnomeName, SUM(quantity) AS [Total gnomes sold]
+-- FROM Gnome G, GnomePurchase GP
+-- WHERE G.gnomeID = GP.gnomeID and G.description LIKE "%solar%"
+-- GROUP BY gnomeName;
+
+-- 2c: Lists email, order number, quantity for most expensive gnome
+SELECT gnomeID
+FROM Gnome
+WHERE unitPrice = 
+(   SELECT MAX(unitPrice)
+    FROM Gnome
+);
