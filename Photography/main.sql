a79fa249631d2c8e70f5c93411e13ca6aabf2849
Jack <J.Walker-35@sms.ed.ac.uk>
2024-12-03 09:20:22 +0000

    Mock DB Test

--- Photography/main.sql
+++ Photography/main.sql
@@ -1,1 +1,11 @@
+<<<<<<< HEAD
 SELECT photographer_id FROM Photographers WHERE experience_years > 5;
+=======
+SELECT name
+FROM Clients
+WHERE client_id IN (
+    SELECT client_id
+    FROM Bookings
+    WHERE event_type = 'Wedding'
+);
+>>>>>>> 6c99ea2 (Mock DB Test)
