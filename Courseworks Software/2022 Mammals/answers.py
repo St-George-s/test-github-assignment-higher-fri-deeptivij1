b67647111f2e9f28fa81e10107579ef7b47835be
deeptivij1 <v2957d@stge.org.uk>
2025-02-03 12:04:40 +0000

    -=#	modified:   Courseworks Software/2022 Mammals/2022.pdf

--- Courseworks Software/2022 Mammals/answers.py
+++ Courseworks Software/2022 Mammals/answers.py
@@ -15,15 +15,16 @@ def readData():
     sightings = []
 
     with open("Courseworks Software/2022 Mammals/mammals.txt", 'r') as file:
-        reader = csv.reader(file) #Reading from CSV 
+        reader = csv.reader(file) #Reading from CSV (don't need to skip header but might have to)
         for row in reader:
             # Creates a sighting object for each row of info 
             # + adds it to array of records
-            sighting_row = sighting(row[0], row[1], row[2], int(row[3]))
+            sighting_row = sighting(row[0], row[1], row[2], int(row[3])) # Check for any integers
             sightings.append(sighting_row)
 
     return sightings
 
+# Find and display oldest walker's age
 def findOldest(sightings):
     maxSighting = sightings[0] # setting highest sighting to whole of record 1
     for sighting in sightings:
@@ -32,25 +33,30 @@ def findOldest(sightings):
 
     print(maxSighting.age) # just print age of record
 
+# Function to convert word into word starting with upper-case character
 def upperCase(word):
     firstChar = ord(word[0])
-    if firstChar >= 97 and firstChar <= 122:
+    if firstChar >= 97 and firstChar <= 122: # if firstChar is uppercase, make it lowercase
         firstChar = firstChar - 32
-        word = chr(firstChar) + word[1:]
-        print(word)
+        word = chr(firstChar) + word[1:] # can't overwrite string indexes - concatenation of character + rest of word
+    
     return word
 
-
-
+# Find and displays dates of sightings of chosen mammal and town
 def displayDates(sightings):
-    town = input("Enter town: ")
+    searchTown = input("Enter town: ") # gets searchTown
+    searchTown = upperCase(searchTown) # uses function to convert searchTown into uppercase
+    searchMammal = input("Enter mammal: ")
+    searchMammal = upperCase(searchMammal)
+    print("The dates of sightings were:")
+    for sighting in sightings:
+        if sighting.town == searchTown and sighting.mammal == searchMammal: # if these match
+            print(sighting.date) # print the date and repeat loop
 
-
+# Count and display
 def numSightings(sightings):
     pass
 
 
-# sightings = readData()
-# findOldest(sightings)
-
-upperCase("hello")
+sightings = readData()
+print(sightings[0])
