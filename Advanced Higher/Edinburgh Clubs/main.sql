8785950b0476fd18daa2a7ce3735f152b03c2e23
deeptivij1 <v2957d@stge.org.uk>
2025-09-23 12:42:56 +0000

    created tables

--- Advanced Higher/Edinburgh Clubs/main.sql
+++ Advanced Higher/Edinburgh Clubs/main.sql
@@ -1,23 +1,23 @@
 -- CREATE DATABASE SwimClubDB;
 USE SwimClubDB;
 
--- CREATE TABLE Centre(
---     centreID INT NOT NULL PRIMARY KEY,
---     centreName VARCHAR(40) NOT NULL,
---     centreType VARCHAR(20) NOT NULL
--- )
+CREATE TABLE Centre(
+    centreID INT NOT NULL PRIMARY KEY,
+    centreName VARCHAR(40) NOT NULL,
+    centreType VARCHAR(20) NOT NULL
+);
 
--- CREATE TABLE Class(
---     classCode VARCHAR(4) NOT NULL PRIMARY KEY,
---     className VARCHAR(40) NOT NULL,
---     centreID INT NOT NULL,
---     level INT NOT NULL,
---     termStartDate DATE,
---     sessionType VARCHAR(12) NOT NULL,
---     startTime TIME NOT NULL,
---     pricePerPerson DECIMAL(6,2) NOT NULL,
---     FOREIGN KEY (centreID) REFERENCES Centre(centreID)
--- )
+CREATE TABLE Class(
+    classCode VARCHAR(4) NOT NULL PRIMARY KEY,
+    className VARCHAR(40) NOT NULL,
+    centreID INT NOT NULL,
+    level INT NOT NULL,
+    termStartDate DATE,
+    sessionType VARCHAR(12) NOT NULL,
+    startTime TIME NOT NULL,
+    pricePerPerson DECIMAL(6,2) NOT NULL,
+    FOREIGN KEY (centreID) REFERENCES Centre(centreID)
+);
 
 CREATE TABLE Member(
     memberNo INT NOT NULL PRIMARY KEY,
@@ -27,13 +27,18 @@ CREATE TABLE Member(
     town VARCHAR(30) NOT NULL,
     postcode VARCHAR(10) NOT NULL
 
-)
+);
 
-CREATE TABLE Booking(
-    classCode INT NOT NULL, 
-    memberNo INT NOT NULL,
-    startDate DATE NOT NULL,
-    numberOfSessions INT NOT NULL,
-    numberInParty INT NOT NULL
-    PRIMARY KEY(classCode, memberNo, startDate)
-)
+CREATE TABLE Booking ( 
+  classCode VARCHAR(4) NOT NULL, 
+  memberNo INT NOT NULL, 
+  startDate DATE NOT NULL, 
+  numberOfSessions INT NOT NULL, 
+  numberInParty INT NOT NULL, 
+  PRIMARY KEY (memberNo, classCode, startDate), 
+  FOREIGN KEY (memberNo) REFERENCES Member(memberNo), 
+  FOREIGN KEY (classCode) REFERENCES Class(classCode) 
+
+); 
+
+
