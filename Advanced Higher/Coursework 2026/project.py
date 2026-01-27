1c70e3712825e3a6da4e1bd17012df47dc7c9f2e
deeptivij1 <v2957d@stge.org.uk>
2026-01-27 13:46:17 +0000

    finishing validating date

--- Advanced Higher/Coursework 2026/project.py
+++ Advanced Higher/Coursework 2026/project.py
@@ -40,27 +40,61 @@ def displayMenu():
     print("4. Find most available doctors")
 
 def validateDOB():
-    inputDOB = input("Enter your date of birth (YYYY-MM-DD):")
-    year = int(inputDOB[:4])
-    month = int(inputDOB[5:7])
-    day = int(inputDOB[8:10])
-    isMonthValid =  not (1 <= month <= 12)
+    valid = False
+    while valid == False:
+        inputDOB = input("Enter your date of birth (YYYY-MM-DD):")
+        year = int(inputDOB[:4])
+        month = int(inputDOB[5:7])
+        day = int(inputDOB[8:10])
 
-
-    while isMonthValid and 
+        # Day check
+        if not (1<= day <= 31):
+            print("Invalid day. Try again.")
+            continue
         
+        # Month check
+        if not (1<= month <= 12):
+            print("Invalid month. Try again.")
+            continue
 
-    # while int() > 2025 or (int(inputDOB[:3]) and int(inputDOB[5:6]) == 12 and int(inputDOB)[8:9] > 5):
-    #     inputDOB = input("Enter your date of birth (YYYY-MM-DD):")
-    #     print("Invalid date of birth.")
-    # return inputDOB
+        # Year check
+        if year < 2025:
+            valid = True
+
+        elif year == 2025:
+            if month == 12 and day >= 5:
+                print("Invalid. DOB must be before 05/12/2025.")
+                continue
+        elif year > 2025:
+            print("Invalid. DOB must be before 05/12/2025.")
+            continue
+        else:
+            valid = True
+    return inputDOB
+  
 
 def validateApptDate():
-    inputDate = int(input("Enter the desired appointment date (YYYY-MM-DD):"))
-    #while inputDate < 2025-12-
+    valid = False
+    while valid == False:
+        inputDate = input("Enter desired appointment date (YYYY-MM-DD):")
+        year = int(inputDate[:4])
+        month = int(inputDate[5:7])
+        day = int(inputDate[8:10])
+
+        # Checks between 01/12/2025 - 05/12/2025
+        if not (1<= day <= 5) and month != 12 and year != 2025:
+            print("Date not in range.")
+            continue
+
+        # Month check
+        if not (1<= month <= 12):
+            print("Invalid month. Try again.")
+            continue
+
 
 date = validateDOB()
 print(date)
+
 
 
 
